e4d7df798ad383e050e4d1c284b51bb38be25dba
MrPandir <MrPandir@users.noreply.github.com>
2024-01-26 16:53:31 +0100

    feat: add error handling for text generation in voice

---
+++ openapi_responses.py
@@ -0,0 +1,37 @@
+GENERATE_RESPONSES = {
+    200: {"content": {"audio/wav": {}}},
+    404: {
+        "content": {
+            "application/json": {
+                "example": {
+                    "status_code": 404,
+                    "detail": "Model not found for speaker: {speaker_name}",
+                }
+            }
+        },
+        "description": "Model not found for speaker",
+    },
+    400: {
+        "content": {
+            "application/json": {
+                "example": {
+                    "status_code": 400,
+                    "detail": "Invalid sample rate: {sample_rate}. Use 8 000, 24 000 or 48 000",
+                }
+            }
+        },
+        "description": "Invalid sample rate",
+    },
+    422: {
+        "content": {
+            "application/json": {
+                "example": {
+                    "status_code": 422,
+                    "detail": "Text not correct: {text}",
+                }
+            }
+        },
+        "description": "Text not correct",
+    },
+}
+
--- server.py
+++ server.py
@@ -1,22 +1,31 @@
 from typing import Annotated
 
-from fastapi import FastAPI, Response
+from fastapi import FastAPI, Response, HTTPException, status
 
 from tts import tts
 from openapi_examples import TextExamples, SpeakerExamples, SampleRateExamples
-
+from openapi_responses import GENERATE_RESPONSES
+from exceptions import NotFoundModelException, NotCorrectTextException
 
 app = FastAPI()
 
 
-@app.get("/generate")
+@app.get("/generate", responses=GENERATE_RESPONSES)
 def generate(
     text: Annotated[str, TextExamples],
     speaker: Annotated[str, SpeakerExamples],
     sample_rate: Annotated[int, SampleRateExamples] = 48_000,
 ):
-    audio = tts.generate(text, speaker, sample_rate)
-    return Response(audio, media_type="audio/wav")
+    if sample_rate not in (8_000, 24_000, 48_000):
+        raise HTTPException(status.HTTP_400_BAD_REQUEST, detail=f"Invalid sample rate: {sample_rate}. Use 8 000, 24 000 or 48 000")
+    try:
+        audio = tts.generate(text, speaker, sample_rate)
+    except NotFoundModelException as error:
+        return HTTPException(status.HTTP_404_NOT_FOUND, detail=str(error))
+    except NotCorrectTextException as error:
+        return HTTPException(status.HTTP_422_UNPROCESSABLE_ENTITY, detail=str(error))
+    else:
+        return Response(audio, media_type="audio/wav")
 
 
 @app.get("/speakers")

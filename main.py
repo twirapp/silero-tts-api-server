1b86547575db3861926ce14ed5ea6d52e1adfef8
MrPandir <MrPandir@users.noreply.github.com>
2024-02-05 13:11:52 +0100

    ops: remove sentry handler

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from dotenv import load_dotenv
-import sentry_sdk
 
 
 def run_server():
@@ -8,7 +7,6 @@ def run_server():
     uvicorn.run("server:app", host="localhost", port=8000)
 
 load_dotenv()
-sentry_sdk.init()
 
 if __name__ == "__main__":
     run_server()
